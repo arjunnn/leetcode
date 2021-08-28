b37c0b0cfd4e9316f1aca95f7afb6ca41a4277c2
Arjun Munji <arjunmunji@gmail.com>
2021-08-29 00:32:40 +0530

    Update longest-common-prefix.py

--- python/longest-common-prefix.py
+++ python/longest-common-prefix.py
@@ -1,16 +1,19 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = list(set(strs))
+        smallest = len(min(strs, key=len))
+        strs = [word[:smallest] for word in strs]
         if len(strs) == 1:
             return strs[0]
-        common_prefix = ""
-        length = len(common_prefix) or 1
+        prefix = ""
+        length = len(prefix) or 1
         while True:
             if len(set([word[:length] for word in strs])) == 1:
-                common_prefix = strs[0][:length]
-                if len(common_prefix) == len(strs[0]):
+                prefix = strs[0][:length]
+                if len(prefix) == len(strs[0]):
                     return strs[0]
-                if common_prefix:
+                if prefix:
                     length += 1
                     continue
             break
-        return common_prefix
+        return prefix
