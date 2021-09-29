7d929d1e002dffc6ef1b69fd02107827f2eb26c7
Arjun Munji <arjunmunji@gmail.com>
2021-09-30 01:00:46 +0530

    Update remove-element.py

--- python/remove-element.py
+++ python/remove-element.py
@@ -1,7 +1,18 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        try:
-            while True:
-                nums.pop(nums.index(val))
-        except ValueError:
-            return len(nums)
+        # try:
+        #     while True:
+        #         nums.pop(nums.index(val))
+        # except ValueError:
+        #     return len(nums)
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start] = nums[end]
+                nums[end] = nums[start]
+                end -= 1
+            else:
+                start += 1
+            print(nums)
+        return start
