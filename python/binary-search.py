26b1f92853fb774a86236d9411bbc4d39e7025c5
Arjun Munji <arjun.munji@synup.com>
2022-10-04 16:02:20 +0530

    Update binary-search.py

--- python/binary-search.py
+++ python/binary-search.py
@@ -3,4 +3,19 @@ class Solution:
         if target in nums:
             return nums.index(target)
         return -1
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
         
