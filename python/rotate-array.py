0a4f1dbb7256c1c2d408f3128aa68f6129f85f79
Arjun Munji <arjunmunji@gmail.com>
2023-09-20 01:59:48 +0100

    Update rotate-array.py

--- python/rotate-array.py
+++ python/rotate-array.py
@@ -1,11 +1,18 @@
+from typing import List
+
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while k:
-            last = nums.pop()
-            nums.insert(0, last)
-            k -= 1
-
-        return nums
+        if k % len(nums) == 0:
+            if k % 2 == 0:
+                pass
+            else:
+                nums = reversed(nums)
+        else:
+            while k:
+                num = nums.pop()
+                nums.insert(0, num)
+                k -= 1
