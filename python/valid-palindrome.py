0bdf5b1faefc0bdc361723ceb88b81948e1c774d
Arjun Munji <arjunmunji@gmail.com>
2023-09-25 22:34:06 +0100

    Update valid-palindrome.py

--- python/valid-palindrome.py
+++ python/valid-palindrome.py
@@ -2,14 +2,13 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         pattern = re.compile('[a-z0-9]+', re.IGNORECASE)
         matched = pattern.findall(s)
-        s = ''.join(matched).lower()
-        print(s)
+        s = ''.join(matched)
         if len(s) == 1:
             return True
         i = 0
         j = len(s) - 1
         while i <= j:
-            if s[i] != s[j]:
+            if s[i].lower() != s[j].lower():
                 return False
             i += 1
             j -= 1
