ebb9d132b70bdef60f20580cce670947984b9320
Arjun Munji <arjunmunji@gmail.com>
2023-09-26 22:41:56 +0100

    Update length-of-last-word.rs
    add another solution

--- rust/length-of-last-word.rs
+++ rust/length-of-last-word.rs
@@ -9,3 +9,21 @@ impl Solution {
             .len() as i32;
     }
 }
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let mut i = 0;
+        let mut start = false;
+        for (index, c) in s.chars().rev().enumerate() {
+            if c != ' ' {
+                i += 1;
+                start = true;
+            } else if c == ' ' && start {
+                return i;
+            }
+        }
+        return i;
+    }
+}
+
+
