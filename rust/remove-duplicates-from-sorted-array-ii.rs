3b1992692ab6dcdb7e322b46db53e3a3b4487e7f
Arjun Munji <arjunmunji@gmail.com>
2023-09-26 15:43:16 +0100

    Update remove-duplicates-from-sorted-array-ii.rs
    simpler solution

--- rust/remove-duplicates-from-sorted-array-ii.rs
+++ rust/remove-duplicates-from-sorted-array-ii.rs
@@ -19,3 +19,20 @@ impl Solution {
         return nums.len() as i32;
     }
 }
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut i = 0;
+        if nums.len() == 1 {
+            return 1;
+        }
+        while i < nums.len() - 2 {
+            if nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2] {
+                let _ = nums.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+        return nums.len() as i32;
+    }
+}
