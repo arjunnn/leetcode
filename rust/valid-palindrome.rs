46b484b99a93faac1245021ea111c9da6cc40b27
Arjun Munji <arjunmunji@gmail.com>
2023-09-26 21:26:52 +0100

    Update valid-palindrome.rs
    another faster solution

--- rust/valid-palindrome.rs
+++ rust/valid-palindrome.rs
@@ -28,3 +28,36 @@ impl Solution {
         return true;
     }
 }
+
+impl Solution {
+    fn is_alpha_numeric(c: char) -> bool {
+        return match c {
+            'a'..='z' => true,
+            'A'..='Z' => true,
+            '0'..='9' => true,
+            _ => false,
+        };
+    }
+
+    pub fn is_palindrome(s: String) -> bool {
+        let an: Vec<char> = s.chars().collect();
+        let mut i = 0;
+        let mut j = an.len() - 1;
+        while j > i {
+            if !Solution::is_alpha_numeric(an[i]) {
+                i += 1;
+                continue;
+            }
+            if !Solution::is_alpha_numeric(an[j]) {
+                j -= 1;
+                continue;
+            }
+            if an[i].to_ascii_lowercase() != an[j].to_ascii_lowercase() {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+}
