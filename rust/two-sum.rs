4cb89211704875b71efa613b8d206f2061110883
Arjun Munji <arjunmunji@gmail.com>
2023-09-27 23:43:51 +0100

    Update two-sum.rs
    add new solution using two pointers

--- rust/two-sum.rs
+++ rust/two-sum.rs
@@ -14,3 +14,21 @@ impl Solution {
         Vec::new()
     }
 }
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut i = 0;
+        let mut j = nums.len() - 1;
+        while i < j {
+            let sum = nums[i] + nums[j];
+            if sum == target {
+                return Vec::from([(i + 1) as i32, (j + 1) as i32]);
+            } else if sum < target {
+                i += 1;
+            } else {
+                j -= 1;
+            }
+        }
+        Vec::new()
+    }
+}
